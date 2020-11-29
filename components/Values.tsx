196f348dc75fad8d0999120de6bb7d7bf2dfd8e0
Pavlo Kyrylenko <pkyrylenko@vector-software.com>
2020-11-29 21:39:35 +0200

    experimenting with controls

--- components/Values.tsx
+++ components/Values.tsx
@@ -3,7 +3,6 @@ import IProduct from '../types/IProduct';
 
 const Values = ({ product }: { product: IProduct }) => {
     //console.log('product: ', product);
-
     /* useEffect(() => {
         setValue(product.glass);
     }, [product.glass]) */
@@ -14,19 +13,23 @@ const Values = ({ product }: { product: IProduct }) => {
         setValue(e.target.value);
     };
 
-    return <div>
-        <input type='number'
+    return <div style={{ marginTop: '30px', marginBottom: '30px' }}>
+        <div>
+            <span>{value} g</span>
+            <span> = </span>
+            <span>{(value / product.glass).toFixed(2)} glasses</span>
+        </div>
+        <input type='range'
+            style={{ margin: '10px' }}
+            id='grams'
+            name='grams'
             value={value}
             min={10}
-            max={1000}
+            max={product.glass * 3}
             step={10}
             autoFocus={true}
             onChange={handleChange}>
         </input>
-        <span> g</span>
-        <p>=</p>
-        <span>{(value / product.glass).toFixed(2)}</span>
-        <span> glass</span>
     </div>
 }
 
