f6d46eec4c1d5cbdb34a39e99c013c01fdf0cb6b
Pavlo Kyrylenko <pkyrylenko@vector-software.com>
2020-12-02 10:11:43 +0200

    added react-select to pick the product

--- pages/[product].tsx
+++ pages/[product].tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Select from 'react-select';
 import Layout from '../components/Layout';
 import Values from '../components/Calculator';
 import products, { getProduct } from '../services/products';
 import IProduct from '../types/IProduct';
 
-export const siteTitle = 'grams in glasses'
+export const siteTitle = 'grams in glasses';
+
+const options = Object.keys(products).map(p => ({ value: p, label: p }));
+
+const customStyles = {
+    container: () => ({
+        width: '90%',
+        position: 'relative',
+        boxSizing: 'border-box'
+    }),
+}
 
 const Product = (props: any) => {
     const router = useRouter();
-    const { locales, defaultLocale } = router;
+    const { locale, locales, defaultLocale } = router;
     const { product }: { product: IProduct } = props;
+
+    const handleChange = (option: any) => router.push(option.value, option.value, { locale: locale });
 
     return <Layout title={`${siteTitle}: ${product.name}`}>
         <Head>
@@ -20,17 +32,21 @@ const Product = (props: any) => {
                 ? <link rel='alternate' hrefLang={x} href={`https://cookulator.vercel.app/${product.name}`} key={x} />
                 : <link rel='alternate' hrefLang={x} href={`https://cookulator.vercel.app/${x}/${product.name}`} key={x} />)}
             <title>{`${siteTitle}: ${product.name}`}</title>
-        </Head>        
+        </Head>
 
-        <ul>
-            {Object.keys(products).map(p => <li key={p}>
-                <Link href={`/${p}`}><a>{p}</a></Link>
-            </li>)}
-        </ul>
+        <Select
+            instanceId='product-list'
+            name='product-list'
+            isSearchable={true}
+            styles={customStyles}
+            onChange={handleChange}
+            options={options}
+            defaultValue={{ value: product.name, label: product.name }}
+        />
 
         <div>{`${product.glass} g = 1 glass`}</div>
 
-        <Values product={product} />        
+        <Values product={product} />
     </Layout>
 }
 //ref: https://nextjs.org/docs/advanced-features/i18n-routing#dynamic-getstaticprops-pages
