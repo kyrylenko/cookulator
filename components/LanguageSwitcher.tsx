6ece4d030ecab3873a06b3220a647980412356c0
Pavlo Kyrylenko <pkyrylenko@vector-software.com>
2020-12-02 16:18:02 +0200

    revert

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -9,7 +9,7 @@ const LanguageSwitcher = (props: any) => {
 
     const handleChange = (e: any) => router.push(path, path, { locale: e.target.value });
 
-    return <select role='navigation' onChange={handleChange} defaultValue={locale} className='select-css'>
+    return <select role='navigation' onChange={handleChange} defaultValue={locale}>
         {locales?.map(x => <option key={x} value={x} lang={x}>{x}</option>)}
     </select>
 }
